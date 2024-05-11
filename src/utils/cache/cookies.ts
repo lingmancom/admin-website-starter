3cc15560cd9e55b7957a918d6c024b959be9dbf5
阿杰 <53169820+laihaojie@users.noreply.github.com>
2024-05-11 18:45:35 +0800

    chore: update

--- src/utils/cache/cookies.ts
+++ src/utils/cache/cookies.ts
@@ -1,14 +1,9 @@
-/** 统一处理 Cookie */
-
-import Cookies from 'js-cookie'
-import CacheKey from '@/constants/cache-key'
-
 export function getToken() {
-  return Cookies.get(CacheKey.TOKEN)
+  return localGet('token')
 }
 export function setToken(token: string) {
-  Cookies.set(CacheKey.TOKEN, token)
+  localSet('token', token)
 }
 export function removeToken() {
-  Cookies.remove(CacheKey.TOKEN)
+  localRemove('token')
 }
